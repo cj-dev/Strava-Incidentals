d9de445b634717fef77dd9afda93809ec66950c5
Chris J <chris@zero1many.net>
2017-08-20 01:39:09 -0400

    Can now draw directions on map with two clicks

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,13 +1,67 @@
-function initMap() {
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer;
-  var map = new google.maps.Map(document.getElementById('map'), {
-	zoom: 7,
-	center: {lat: 41.85, lng: -87.65}
-  });
-  directionsDisplay.setMap(map);
+var directionsDisplay;
+var directionsService;
+var map;
 
-  var onChangeHandler = function() {
-	calculateAndDisplayRoute(directionsService, directionsDisplay);
-  };
+var startMarker;
+var endMarker;
+
+
+function initMap() {
+    directionsService = new google.maps.DirectionsService;
+    directionsDisplay = new google.maps.DirectionsRenderer({draggable: true});
+    var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+    var mapOptions = {
+          zoom:7,
+          center: chicago
+        }
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    directionsDisplay.setMap(map);
+
+    google.maps.event.addListener(map, "click", function(event) {
+        addLocation(event.latLng);
+    })
+
+}
+
+function addLocation(latLng) {
+    if(startMarker === undefined) {
+        addStartLocation(latLng);
+    }
+    else if (endMarker === undefined) {
+        addEndLocation(latLng);
+        calcRoute(startMarker, endMarker)
+    }
+    // else {
+    //     addWaypoint(latLng);
+    // }
+}
+function addStartLocation(latLng) {
+    startMarker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        title: 'Start'
+    });
+
+}
+function addEndLocation(latLng) {
+    endMarker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        title: 'Finish'
+    });
+}
+function calcRoute(start, end) {
+    var request = {
+        origin: start.position,
+        destination: end.position,
+        travelMode: 'BICYCLING'
+    };
+    directionsService.route(request, function(result, status) {
+        if (status == 'OK') {
+            directionsDisplay.setDirections(result);
+        }
+        else {
+            console.log("Something bad happened determining directions: " + status);
+        }
+    });
 }
